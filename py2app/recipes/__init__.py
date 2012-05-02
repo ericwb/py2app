e61ceb58c0ef265c6281a3c923ca5b3697a3efe6
Ronald Oussoren <ronaldoussoren@mac.com>
2012-05-02 12:58:56 +0200

    Change syntax of simple example: it is now valid python 3 as wel as python 2

---
+++ py2app/recipes/__init__.py
@@ -0,0 +1,14 @@
+import PIL
+import pygame
+import pydoc
+import docutils
+import pyopengl
+import sip
+
+import numpy
+import scipy
+import matplotlib
+
+import virtualenv
+import pyside
+import wx
