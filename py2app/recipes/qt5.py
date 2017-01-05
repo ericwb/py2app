d450f72e30cd3c199675ac6bc375b7a702f2dda6
Armin Samii <armin.samii@gmail.com>
2017-01-05 11:23:49 -0800

    Use the correct copy_file command

---
+++ py2app/recipes/qt5.py
@@ -0,0 +1,21 @@
+def check(cmd, mf):
+    m = mf.findNode('PyQt5')
+    if m:
+        try:
+            # PyQt5 with sipconfig module, handled
+            # by sip recipe
+            import sipconfig
+            return None
+
+        except ImportError:
+            pass
+
+        # All imports are done from C code, hence not visible
+        # for modulegraph
+        # 1. Use of 'sip'
+        # 2. Use of other modules, datafiles and C libraries
+        #    in the PyQt5 package.
+        mf.import_hook('sip', m)
+        return dict(packages=['PyQt5'])
+
+    return None
